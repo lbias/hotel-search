a944df5f6e948c7ac6df6baa29382a0b4720f47d
lbias <306508354@qq.com>
2017-08-03 00:56:04 +0800

    model supplier, controller suppliers crud

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -1,2 +1,55 @@
 class Api::V1::SuppliersController < ApiController
+  before_action :find_supplier, only: [:show, :update, :destroy]
+
+  def index
+    @suppliers = Supplier.all
+    render json: @suppliers
+  end
+
+  def show
+    render json: @supplier
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    if @supplier.save
+      redirect_to @supplier, notice: 'Supplier was successfully created.'
+      render json: :show, status: :created, location: @supplier
+    else
+      render :new
+      render json: @supplier.errors, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @supplier.update(supplier_params)
+      redirect_to @supplier, notice: 'Supplier was successfully updated.'
+      render json: :show, status: :ok, location: @supplier
+    else
+      render :edit
+      render json: @supplier.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @supplier.destroy
+    redirect_to suppliers_url, notice: 'Supplier was successfully destroyed.'
+  end
+
+  private
+    def find_supplier
+      @supplier = Supplier.find(params[:id])
+    end
+
+    def supplier_params
+      params.fetch(:supplier, {})
+    end
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,14 @@
+require 'open-uri'
+
 class Supplier < ApplicationRecord
+  def fetch_supplier
+    uri = URI.parse(url).read
+    JSON.parse(uri).map do |id, price|
+      {id: id, price: price, supplier: name}
+    end
+  end
+
+  def Supplier.find_by_name_of obj
+    where(name: obj)
+  end
 end
