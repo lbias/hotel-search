a284b9263740d0b86d9b506fa044fba0a2e7aad3
lbias <306508354@qq.com>
2017-08-02 23:11:55 +0800

    model supplier, seed

---
+++ db/migrate/20170802150343_create_suppliers.rb
@@ -0,0 +1,9 @@
+class CreateSuppliers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :suppliers do |t|
+      t.string :name, null: false
+      t.text   :url, null: false        
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Supplier.create!([
+  {name: "supplier1", url: "https://api.myjson.com/bins/2tlb8"},
+  {name: "supplier2", url: "https://api.myjson.com/bins/42lok"},
+  {name: "supplier3", url: "https://api.myjson.com/bins/15ktg"}
+])
